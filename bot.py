ebd56d1cf89562ac89df69d42cb2dc37f2862f3e
Maynard <elimaynard923@gmail.com>
2020-12-07 17:04:39 -0800

    don't store token in the code itself whoops

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import logging
 
 from telegram import Update
@@ -16,6 +17,18 @@ with open('./words.txt', 'r') as f:
   prohibited_words = set(f.read().strip().split('\n'))
 
 
+def get_bot_token():
+  token_file = './token.txt'
+  if os.path.isfile(token_file):
+    with open(token_file, 'r') as f:
+      return f.read().strip()
+  else:
+    token = input('Token: ')
+    with open(token_file, 'w') as f:
+      f.write(token + '\n')
+    return token
+
+
 def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
     if set(update.message.text.split(' ')) & prohibited_words > set():
@@ -23,7 +36,7 @@ def echo(update: Update, context: CallbackContext) -> None:
 
 
 def main():
-    updater = Updater("1464169021:AAE2NbcxNw16UukM8QYxgqV8qV7IvYs0abM", use_context=True)
+    updater = Updater(get_bot_token(), use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
     updater.start_polling()
